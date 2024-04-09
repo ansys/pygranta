3534108d73dcf210ed9839e136a111ea7b1b982c
Andy Grigg <andrew.grigg@ansys.com>
2024-04-09 12:06:25 -0400

    Set user agent

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -87,3 +87,8 @@ source_suffix = ".rst"
 
 # The master toctree document.
 master_doc = "index"
+
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81"
+)
