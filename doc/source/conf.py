133bc8821a4e40ee22ba89bd352662da87ddfade
Andy Grigg <andrew.grigg@ansys.com>
2024-04-09 12:22:03 -0400

    Set a more correct user agent

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,8 @@
 """Sphinx documentation configuration file."""
+
 from datetime import datetime
 import os
+import platform
 
 from ansys_sphinx_theme import ansys_favicon, get_version_match, pyansys_logo_black
 
@@ -88,7 +90,11 @@ source_suffix = ".rst"
 # The master toctree document.
 master_doc = "index"
 
+# Set a custom user agent for linkcheck purposes
+python_implementation = platform.python_implementation()
+python_version = platform.python_version()
+os_version = platform.platform()
 user_agent = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81"
+    f"PyGranta Sphinx Documentation/{release} {python_implementation}/{python_version} "
+    f"({os_version})"
 )
